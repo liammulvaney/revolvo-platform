9d5d41f784bc658de25c4524bd307e5ac342e275
Liam Mulvaney <camagakhan@gmail.com>
2025-08-02 11:16:50 +0200

    Set up the config module and added the path to the authentication lib

--- revolvo-backend/apps/auth-api/src/app/app.module.ts
+++ revolvo-backend/apps/auth-api/src/app/app.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '@revolvo-backend/authentication';
+import { ConfigModule } from '@nestjs/config';
+import { AuthGuard, RevolvoJWTBackendAuthenticationModule } from '@revolvo-backend/authentication';
+import path from 'path';
+
+// Ensure the environment variables are loaded from the .env file
+const envFilePath = path.resolve(__dirname, '../../../env/.env');
+console.log(`variables: ${envFilePath}`);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the configuration globally available
+      envFilePath, // Load environment variables from .env file
+    }), 
+    // set up the authentication module with JWT options
+    RevolvoJWTBackendAuthenticationModule.register({
+      signOptions: { expiresIn: '1h' },
+      privateKey: process.env.PRIVATE_JWT_KEY_DIRECTORY || 'test', // Use your private key directory
+      publicKey: process.env.PUBLIC_JWT_KEY_DIRECTORY || 'test', // Use your public key directory
+    })
+],
   controllers: [AppController],
   providers: [
     {
